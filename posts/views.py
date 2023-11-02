d47af2efa95e277ce45b76f6cf00395cb0c98b71
maclen2007 <maclen2007@yandex.ru>
2023-11-02 13:29:18 +0300

    Создание базового шаблона

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Post
 
 def index(request):
     latest = Post.objects.order_by('-pub_date')[:10]
-    output = []
-    for item in latest:
-        output.append(item.text)
-    return HttpResponse('\n'.join(output))
+    return render(request, "index.html", {"posts": latest})
