deb1589c4af9d766635351ce493fb4ed3c932c1f
maclen2007 <maclen2007@yandex.ru>
2023-11-02 12:46:06 +0300

    Вывод всех записей блога

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Post
+
+def index(request):
+    latest = Post.objects.order_by('-pub_date')[:10]
+    output = []
+    for item in latest:
+        output.append(item.text)
+    return HttpResponse('\n'.join(output))
