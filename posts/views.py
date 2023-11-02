7a6c2e8733a608b4496f1d3ddf36e56aa4c572e7
maclen2007 <maclen2007@yandex.ru>
2023-11-02 13:54:50 +0300

    Шаблоны списка постов и отдельного поста

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Post
 
 def index(request):
     latest = Post.objects.order_by('-pub_date')[:10]
     return render(request, "index.html", {"posts": latest})
+
+def post_view(request, post_id):
+  post = get_object_or_404(Post, id=post_id)
+  context = {
+    'post': post,
+  }
+  return render(request, 'post.html', context)
