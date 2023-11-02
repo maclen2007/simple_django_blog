0d4e3bcfac7c7c65482d7c41995cefccc2d773bd
maclen2007 <maclen2007@yandex.ru>
2023-11-02 12:33:00 +0300

    Добавление сущности в админку

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 # Register your models here.
+
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+  list_display = ("pk", "title_href", "pub_date", "author")
+  search_fields = ("text",)
+  list_filter = ("pub_date",)
+
+  def title_href(self, obj):
+    return format_html('<a href="%s">%s</a>' % (obj.id, obj.title))
+  
+  title_href.short_description = "Название статьи"
+
+admin.site.register(Post, PostAdmin)
