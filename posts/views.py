390520692bc21a64434636d44a7c5174d637ce12
maclen2007 <maclen2007@yandex.ru>
2023-11-04 12:43:23 +0300

    Внедрение пагинации

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
 
 from .models import Post
 
+paginator_pages = 1
+
 def index(request):
-    latest = Post.objects.order_by('-pub_date')[:10]
-    return render(request, "index.html", {"posts": latest})
+  latest = Post.objects.order_by('-pub_date')
+  paginator = Paginator(latest, paginator_pages)
+  page = paginator.get_page(request.GET.get('page'))
+  return render(request, "index.html", {"page": page})
 
 def post_view(request, post_id):
   post = get_object_or_404(Post, id=post_id)
